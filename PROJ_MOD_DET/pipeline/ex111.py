083a221fd1e296245fb0cdb8bd6af104b9371d27
Pan-Bodzanowski <160624479+Pan-Bodzanowski@users.noreply.github.com>
2025-02-02 11:51:12 +0100

    Update ex111.py

--- PROJ_MOD_DET/pipeline/ex111.py
+++ PROJ_MOD_DET/pipeline/ex111.py
@@ -2,16 +2,16 @@ import objects
 
 def exp1():
     params = objects.load_params()
-    sim = objects.Simulation(params)
+    sim = objects.Simulation(params,"domyślne parametry")
     sim.run()
     sim.animate()
 
 def exp2():
     params = objects.load_params()
-    params["heater_placement"] = [2.5, 1]
+    params["heater_placement"] = [2,5, 1]
     params["heater_dimensions"] = [0.1, 0.1]
     params["heater_power"] = 250
-    sim = objects.Simulation(params)
+    sim = objects.Simulation(params,"co jeśli grzejnik dać na środku?")
     sim.run()
     sim.animate()
 
@@ -19,9 +19,10 @@ def exp3():
     params = objects.load_params()
     params["heater_placement"] = [1, 2.3]
     params["heater_dimensions"] = [0.4, 0.05]
-    sim = objects.Simulation(params)
+    sim = objects.Simulation(params,"co jeśli grzejnik dać przy ścianie i blisko okna?")
     sim.run()
     sim.animate()
 
+#wybieramy eksperyment
 def exp():
-    exp3()
+    exp1()
